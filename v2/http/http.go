d7c7c4f7bd08e9146919ca6eca72fb3c214403e6
Rajat Jindal <rajatjindal83@gmail.com>
2024-09-01 06:58:46 +0530

    feat(sdk): add support for http trigger with wasip2 support
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

---
+++ v2/http/http.go
@@ -0,0 +1,98 @@
+// Package http contains the helper functions for writing Spin HTTP components
+// in TinyGo, as well as for sending outbound HTTP requests.
+package http
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	incominghandler "github.com/fermyon/spin-go-sdk/v2/internal/wasi/http/v0.2.0/incoming-handler"
+	"github.com/fermyon/spin-go-sdk/v2/internal/wasi/http/v0.2.0/types"
+	"github.com/fermyon/spin-go-sdk/v2/wit"
+	"github.com/julienschmidt/httprouter"
+)
+
+// force wit files to be shipped with sdk dependency
+var _ = wit.Wit
+
+func init() {
+	incominghandler.Exports.Handle = wasiHandle
+}
+
+const (
+	// The application base path.
+	HeaderBasePath = "spin-base-path"
+	// The component route pattern matched, _excluding_ any wildcard indicator.
+	HeaderComponentRoot = "spin-component-route"
+	// The full URL of the request. This includes full host and scheme information.
+	HeaderFullUrl = "spin-full-url"
+	// The part of the request path that was matched by the route (including
+	// the base and wildcard indicator if present).
+	HeaderMatchedRoute = "spin-matched-route"
+	// The request path relative to the component route (including any base).
+	HeaderPathInfo = "spin-path-info"
+	// The component route pattern matched, as written in the component
+	// manifest (that is, _excluding_ the base, but including the wildcard
+	// indicator if present).
+	HeaderRawComponentRoot = "spin-raw-component-route"
+	// The client address for the request.
+	HeaderClientAddr = "spin-client-addr"
+)
+
+// Router is a http.Handler which can be used to dispatch requests to different
+// handler functions via configurable routes
+type Router = httprouter.Router
+
+// Params is a Param-slice, as returned by the router.
+// The slice is ordered, the first URL parameter is also the first slice value.
+// It is therefore safe to read values by the index.
+type Params = httprouter.Params
+
+// Param is a single URL parameter, consisting of a key and a value.
+type Param = httprouter.Param
+
+// RouterHandle is a function that can be registered to a route to handle HTTP
+// requests. Like http.HandlerFunc, but has a third parameter for the values of
+// wildcards (variables).
+type RouterHandle = httprouter.Handle
+
+// New returns a new initialized Router.
+// Path auto-correction, including trailing slashes, is enabled by default.
+func NewRouter() *Router {
+	return httprouter.New()
+}
+
+// handler is the function that will be called by the http trigger in Spin.
+var handler = defaultHandler
+
+// defaultHandler is a placeholder for returning a useful error to stderr when
+// the handler is not set.
+var defaultHandler = func(http.ResponseWriter, *http.Request) {
+	fmt.Fprintln(os.Stderr, "http handler undefined")
+}
+
+// Handle sets the handler function for the http trigger.
+// It must be set in an init() function.
+func Handle(fn func(http.ResponseWriter, *http.Request)) {
+	handler = fn
+}
+
+var wasiHandle = func(request types.IncomingRequest, responseOut types.ResponseOutparam) {
+	// convert the incoming request to go's net/http type
+	httpReq, err := NewHttpRequest(request)
+	if err != nil {
+		//TODO(rajatjindal): return internal error from here
+		fmt.Printf("failed to convert wasi/http/types.IncomingRequest to http.Request: %s\n", err)
+		return
+	}
+
+	// convert the response outparam to go's net/http type
+	httpRes := NewHttpResponseWriter(responseOut)
+
+	// run the user's handler
+	handler(httpRes, httpReq)
+
+	// ensure default status ok and response body are set
+	_ = httpRes.reconcile()
+}
---
+++ v2/integration_test.go
@@ -0,0 +1,152 @@
+package http
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+const spinBinary = "spin"
+
+func retryGet(t *testing.T, url string) *http.Response {
+	t.Helper()
+
+	const maxTries = 600 // (10min)
+	for i := 1; i < maxTries; i++ {
+		// Catch call to `Fail` in other goroutine
+		if t.Failed() {
+			t.FailNow()
+		}
+		if res, err := http.Get(url); err != nil {
+			t.Log(err)
+		} else {
+			return res
+		}
+		time.Sleep(1 * time.Second)
+	}
+	t.Fatal("Get request timeout: ", url)
+	return nil
+}
+
+type testSpin struct {
+	cancel func()
+	url    string
+	cmd    *exec.Cmd
+}
+
+func startSpin(t *testing.T, dir string) *testSpin {
+	buildApp(t, dir)
+
+	url := getFreePort(t)
+
+	// long timeout because... ci
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
+
+	cmd := exec.CommandContext(ctx, spinBinary, "up", "--listen", url)
+	cmd.Dir = dir
+	stderr := new(bytes.Buffer)
+	cmd.Stderr = stderr
+	if err := cmd.Start(); err != nil {
+		t.Log(stderr.String())
+		t.Fatal(err)
+	}
+
+	go func() {
+		_ = cmd.Wait()
+		if ctx.Err() == nil {
+			t.Log("spin exited before the test finished:", cmd.ProcessState)
+			t.Log("stderr:\n", stderr.String())
+			t.Fail()
+		}
+	}()
+
+	return &testSpin{
+		cancel: cancel,
+		url:    fmt.Sprintf("http://%s", url),
+		cmd:    cmd,
+	}
+}
+
+func buildApp(t *testing.T, dir string) {
+	t.Helper()
+
+	t.Log("building application:", dir)
+
+	cmd := exec.Command(spinBinary, "build")
+	cmd.Dir = dir
+
+	stderr := new(bytes.Buffer)
+	cmd.Stderr = stderr
+	if err := cmd.Run(); err != nil {
+		t.Log(stderr.String())
+		t.Errorf("Failed to build %q, %v", dir, err)
+	}
+}
+
+func TestHTTPTriger(t *testing.T) {
+	spin := startSpin(t, "http/testdata/http-tinygo")
+	defer spin.cancel()
+
+	resp := retryGet(t, spin.url+"/hello")
+	spin.cancel()
+	if resp.Body == nil {
+		t.Fatal("body is nil")
+	}
+	t.Log(resp.Status)
+	b, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// assert response body
+	want := "== RESPONSE ==\nHello Fermyon!\nHello again Fermyon!\n"
+	got := string(b)
+	if want != got {
+		t.Fatalf("body is not equal: want = %q got = %q", want, got)
+	}
+
+	// assert response header
+	if resp.Header.Get("foo") != "bar" {
+		t.Fatal("header 'foo' was not set")
+	}
+}
+
+// TestBuildExamples ensures that the tinygo examples will build successfully.
+func TestBuildExamples(t *testing.T) {
+	examples, err := os.ReadDir("examples")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, example := range examples {
+		example := example
+		t.Run(example.Name(), func(t *testing.T) {
+			t.Parallel()
+			buildApp(t, filepath.Join("examples", example.Name()))
+		})
+	}
+}
+
+func getFreePort(t *testing.T) string {
+	t.Helper()
+
+	a, err := net.ResolveTCPAddr("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal("failed to get free port: ", err)
+	}
+
+	l, err := net.ListenTCP("tcp", a)
+	if err != nil {
+		t.Fatal("failed to get free port: ", err)
+	}
+	l.Close()
+	return l.Addr().String()
+}
